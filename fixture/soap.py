a23784816e2ced1205bfd35361194be31a89809e
Dmitriy <dstikhonov@rt-dc.ru>
2021-02-07 21:11:34 +0300

    Реализованы проверки списков проектов через SOAP

---
+++ fixture/soap.py
@@ -0,0 +1,27 @@
+from suds.client import Client
+from suds import WebFault
+from model.project import Project
+
+
+class SoapHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def can_login(self, username, password):
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        try:
+            client.service.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
+
+    def get_project_list(self, username, password):
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+
+        try:
+            soap_projects = client.service.mc_projects_get_user_accessible(username, password)
+        except WebFault:
+            return None
+        projects = [Project(id=x.id, name_project=x.name, description=x.description) for x in soap_projects]
+        return projects
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -3,13 +3,13 @@ from model.project import Project
 
 def test_add_project(app):
     app.session.login("administrator", "root")
-    assert app.session.is_logged_in_as("administrator")
-    old_list_project = app.project.get_project_list()
-    app.project.create()
+    old_list_project = app.soap.get_project_list(username="administrator", password="root")
     project = Project(name_project=str("test"), status="stable",
                       description="test", view_status="public")
+    app.project.create()
     app.project.fill_form_project(project)
     app.project.confirm_add_project()
-    new_list_project = app.project.get_project_list()
-    assert len(old_list_project) == len(new_list_project)
+    new_list_project = app.soap.get_project_list(username="administrator", password="root")
     old_list_project.append(project)
+    assert len(old_list_project) == len(new_list_project)
+
