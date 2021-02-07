aae20e9778eef542440d4229d045146bc0f073fe
Dmitriy <dstikhonov@rt-dc.ru>
2021-02-07 14:18:42 +0300

    Реализованы тесты для удалённого управления по протоколу Telnet

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from fixture.session import sessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
 
 
 class Application:
 
-    def __init__(self, browser, baseUrl):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox(executable_path=r'../geckodriver.exe')
         elif browser == "chrome":
@@ -17,7 +18,9 @@ class Application:
         self.wd.implicitly_wait(3)
         self.session = sessionHelper(self)
         self.project = ProjectHelper(self)
-        self.baseUrl = baseUrl
+        self.james = JamesHelper(self)
+        self.config = config
+        self.baseUrl = config['web']['baseUrl']
 
     def is_valid(self):
         try:
