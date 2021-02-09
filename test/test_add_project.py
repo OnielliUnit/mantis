d4df16ceed601992a2e3d0f946d12c1de1fafb45
Dmitriy <dstikhonov@rt-dc.ru>
2021-02-09 12:50:13 +0300

    Внесены именения в soap.py

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,15 +1,15 @@
 from model.project import Project
 
 
-def test_add_project(app):
+def test_add_project(app, config):
     app.session.login("administrator", "root")
-    old_list_project = app.soap.get_project_list(username="administrator", password="root")
+    old_list_project = app.soap.get_project_list(username=config['web']["username"], password=config['web']["password"], baseUrl=config['web']['baseUrl'])
     project = Project(name_project=str("test"), status="stable",
                       description="test", view_status="public")
     app.project.create()
     app.project.fill_form_project(project)
     app.project.confirm_add_project()
-    new_list_project = app.soap.get_project_list(username="administrator", password="root")
+    new_list_project = app.soap.get_project_list(username=config['web']["username"], password=config['web']["password"], baseUrl=config['web']['baseUrl'])
     old_list_project.append(project)
     assert len(old_list_project) == len(new_list_project)
 
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -1,7 +1,7 @@
 from model.project import Project
 
 
-def test_del_project(app):
+def test_del_project(app, config):
     app.session.login("administrator", "root")
     app.project.open_project()
     if len(app.project.get_project_list()) == 0:
@@ -9,10 +9,10 @@ def test_del_project(app):
         app.project.fill_form_project(Project(name_project=str("test"), status="stable",
                                               description="test", view_status="public"))
         app.project.confirm_add_project()
-    old_list_project = app.soap.get_project_list(username="administrator", password="root")
+    old_list_project = app.soap.get_project_list(username=config['web']["username"], password=config['web']["password"], baseUrl=config['web']['baseUrl'])
     app.project.del_project()
     app.project.open_project()
-    new_list_project = app.soap.get_project_list(username="administrator", password="root")
+    new_list_project = app.soap.get_project_list(username=config['web']["username"], password=config['web']["password"], baseUrl=config['web']['baseUrl'])
     assert len(old_list_project) - 1 == len(new_list_project)
     old_list_project[0:1] = []
     assert old_list_project == new_list_project
